27e3e310f53b5b5732bc6d71e2d76f7706e17896
woodstock1993 <diqkqkqk@gmail.com>
2022-05-02 16:04:59 +0900

    feat: guest model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Guest(models.Model):
+    restuarant = models.ForeignKey()
+    created_at = models.DateTimeField()
+    price = models.PositiveIntegerField()
+    number_of_party = models.PositiveIntegerField()
+    payment = models.Charfield()
