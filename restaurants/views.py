22c733d2c9a8bf87cf90eb98da677963267cd694
Jjenny-K <kkjjh0129@gmail.com>
2022-05-04 12:42:39 +0900

    (fix)Update total_pirce API aggregation

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Q, Min, Max, Sum
+from django.db.models import Q, Sum
 from django.db.models.functions import ExtractYear, ExtractMonth, ExtractWeek, ExtractDay, ExtractHour
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -6,6 +6,85 @@ from rest_framework.response import Response
 
 from restaurants.models import Restaurant, Guest
 from restaurants.serializers import RestaurantSerializer
+
+
+def _request_param(request):
+    """
+        editor : 강정희
+    """
+    result = {
+        'start_time': request.query_params.get('start_time', None),
+        'end_time': request.query_params.get('end_time', None),
+        'timeunit': request.query_params.get('timeunit', None),
+        'min_price': request.query_params.get('min_price', None),
+        'max_price': request.query_params.get('max_price', None),
+        'min_party': request.query_params.get('min_party', None),
+        'max_party': request.query_params.get('max_party', None),
+        'group': request.query_params.get('group', None)
+    }
+    return result
+
+
+def _exception_handling(request):
+    """
+        editor : 강정희
+    """
+    requests = _request_param(request)
+
+    timeunit = requests.get('timeunit')
+    occurred = False
+    error = ''
+
+    query = Q(timestamp__range=(requests.get('start_time'), requests.get('end_time')))
+
+    if requests.get('min_price') and requests.get('max_price'):
+        query &= Q(price__range=(requests.get('min_price'), requests.get('max_price')))
+    if requests.get('min_party') and requests.get('max_party'):
+        query &= Q(number_of_party__range=(requests.get('min_party'), requests.get('max_party')))
+    if requests.get('group'):
+        query &= Q(restaurant__group__name=requests.get('group'))
+
+    # request 입력값에 대한 예외처리
+    if requests.get('start_time') > requests.get('end_time'):
+        occurred = True
+        error = Response({'error_message': "조회 시작 날짜보다 끝 날짜가 빠를 수 없습니다."},
+                         status=status.HTTP_400_BAD_REQUEST)
+
+    if requests.get('min_price') and requests.get('max_price'):
+        if requests.get('min_price').isdigit() or requests.get('max_price').isdigit():
+            occurred = True
+            error = Response({'error_message': "조회 최소 가격, 최대 가격은 숫자값을 입력해야합니다."},
+                             status=status.HTTP_400_BAD_REQUEST)
+        if requests.get('min_price') > requests.get('max_price'):
+            occurred = True
+            error = Response({'error_message': "조회 최소 가격보다 최대 가격이 클 수 없습니다."},
+                             status=status.HTTP_400_BAD_REQUEST)
+
+    if requests.get('min_party') and requests.get('max_party'):
+        if requests.get('min_party').isdigit() or requests.get('max_party').isdigit():
+            occurred = True
+            error = Response({'error_message': "조회 최소 방문자 수, 최대 방문자 수는 숫자값을 입력해야합니다."},
+                             status=status.HTTP_400_BAD_REQUEST)
+        if requests.get('min_party') > requests.get('max_party'):
+            occurred = True
+            error = Response({'error_message': "조회 최소 방문자 수보다 최대 방문자 수가 클 수 없습니다."},
+                             status=status.HTTP_400_BAD_REQUEST)
+
+    if requests.get('group'):
+        group_chk = Guest.objects.filter(query).exists()
+
+        if not group_chk:
+            occurred = True
+            error = Response({'error_message': "조회 그룹 이름이 존재하지 않습니다."},
+                             status=status.HTTP_400_BAD_REQUEST)
+
+    result = {
+        'timeunit': timeunit,
+        'query': query,
+        'occurred': occurred,
+        'error': error
+    }
+    return result
 
 
 class RestaurantViewset(viewsets.ModelViewSet):
@@ -17,43 +96,18 @@ class RestaurantViewset(viewsets.ModelViewSet):
         """
             editor : 강정희
         """
-        start_time = request.query_params.get('start_time', None)
-        end_time = request.query_params.get('end_time', None)
-        timeunit = request.query_params.get('timeunit', None)
-        min_price = request.query_params.get('min_price', None)
-        max_price = request.query_params.get('max_price', None)
-        min_party = request.query_params.get('min_party', None)
-        max_party = request.query_params.get('max_party', None)
-        group = request.query_params.get('group', None)
+        exception_chk = _exception_handling(request)
 
-        # request 입력값에 대한 예외처리
-        if start_time > end_time:
-            return Response({'error_message': "조회 시작 날짜보다 끝 날짜가 빠를 수 없습니다."},
-                            status=status.HTTP_400_BAD_REQUEST)
-        if min_price and max_price and min_price > max_price:
-            print(min_price.isdigit())
-            return Response({'error_message': "조회 시작 날짜보다 끝 날짜가 빠를 수 없습니다."},
-                            status=status.HTTP_400_BAD_REQUEST)
-        # if min_party > max_party:
-        #     return Response({'error_message': "조회 시작 날짜보다 끝 날짜가 빠를 수 없습니다."},
-        #                     status=status.HTTP_400_BAD_REQUEST)
+        if exception_chk.get('occurred'):
+            return exception_chk.get('error')
 
         try:
-            query = Q(timestamp__range=(start_time, end_time))
-
-            if min_price and max_price:
-                query &= Q(price__range=(min_price, max_price))
-            if min_party and max_party:
-                query &= Q(number_of_party__range=(min_party, max_party))
-            if group:
-                query &= Q(restaurant__group__name=group)
+            query = exception_chk.get('query')
 
             guests = Guest.objects.filter(query)
 
-            timeunit = timeunit.upper()
+            timeunit = exception_chk.get('timeunit').upper()
             timeunit_group = ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR']
-            print(timeunit)
-            print(timeunit and timeunit in timeunit_group)
 
             if timeunit and timeunit in timeunit_group:
                 if timeunit == 'YEAR':
@@ -77,8 +131,6 @@ class RestaurantViewset(viewsets.ModelViewSet):
                 return Response({'error_message': "시간 단위는 &timeunit=hour/day/week/month/year' 형식으로 요청 가능합니다."},
                                 status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            print('first try-except')
-            print(e)
             return Response({'error_message': "기간은 'start_time=yyyy-mm-dd 00:00:00&end_time=yyyy-mm-dd 00:00:00"
                                               "&timeunit=hour/day/week/month/year' 형식으로 요청 가능합니다."},
                             status=status.HTTP_400_BAD_REQUEST)
